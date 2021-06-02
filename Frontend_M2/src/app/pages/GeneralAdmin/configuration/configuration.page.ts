e6646b59f4c1d5956523b4a6d15e579a770a466e
sumayabl <>
2021-06-02 09:09:28 +0200

    true&false toggle = OK

--- Frontend_M2/src/app/pages/GeneralAdmin/configuration/configuration.page.ts
+++ Frontend_M2/src/app/pages/GeneralAdmin/configuration/configuration.page.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfigurationPage implements OnInit {
 
+  public notification:boolean = true;
+  public private:boolean = true;
+  public authentication:boolean = true;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  myChange(){
+    console.log(this.notification);
+    console.log(this.private);
+    console.log(this.authentication);
   }
 
 }
